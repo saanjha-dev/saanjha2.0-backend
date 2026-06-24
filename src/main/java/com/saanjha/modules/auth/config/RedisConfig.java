7aa717b6069b6590e28f1a5e269fcee6d757f68e
Sumit Shresht <sumitshresht754@gmail.com>
2026-06-25 02:36:40 +0530

    fix: use redis host from configuration

--- src/main/java/com/saanjha/modules/auth/config/RedisConfig.java
+++ src/main/java/com/saanjha/modules/auth/config/RedisConfig.java
@@ -1,14 +1,21 @@
 package com.saanjha.modules.auth.config;
 
 import io.lettuce.core.RedisClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisClient redisClient() {
-        return RedisClient.create("redis://localhost:6380");
+        return RedisClient.create("redis://" + redisHost + ":" + redisPort);
     }
 }
